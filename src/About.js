54e1e0e1b50ff66f2d3491b32b150307ba34215f
Bulletmagnet <bulletmagnet.123@gmail.com>
2021-06-30 19:58:14 -0700

    second commit

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => (
+ <div>
+     <h1>TESTING FUCKING TEST ABOUT US</h1>
+ </div>
+)
---
+++ src/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Contact = () => (
+ <div>
+     <h1>TESTING FUCKING TEST ABOUT CONTACT</h1>
+ </div>
+)
