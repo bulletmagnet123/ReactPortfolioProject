85dbcbab2d77f480a813868666ea62fbe3932a5d
Nate Phelps <bulletmagnet.123@gmail.com>
2021-07-10 23:20:03 -0700

    Ok now this is the last update for 7/10/2021

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Home } from './Home'
 import { About } from './About'
 import { Contact } from './Contact'
 import { NoMatch } from './NoMatch'
+
 import { Layout } from './components/Layout'
 import { NavigationBar } from './components/NavBar'
 import { Jumbotron } from './components/Jumbotron'
@@ -25,17 +26,13 @@ class App extends Component {
         <Jumbotron />
         <Layout>
           <Router>
-            <TransitionGroup>
-              <CSSTransition classNames="fade" timeout={300}>
                 <Switch>
                   <Route exact path="/" component={Home} />
                   <Route path="/about" component={About} />
                   <Route path="/contact" component={Contact} />
                   <Route component={NoMatch} />
                 </Switch>
-              </CSSTransition>
               <IonPhaser game={gameconfig} />
-            </TransitionGroup>
           </Router>
         </Layout>
       </React.Fragment>
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import '../SideNav.css'
-import { IoHome } from "react-icons/io5";
+import { IoHome } from "react-icons/io5"
 import { Button } from "react-bootstrap"
+import { BiArchive } from "react-icons/bi";
+import { VscFileSubmodule } from "react-icons/vsc";
+
 function SideNav() {
     return (
         <div className="Sidebar">
             <ul>
                 <h4>Links to the websites Mentioned</h4>
                     <li>
-                    <Button color="primary"> <IoHome />
-                    FFXIV Main page
+                    <Button href="https://na.finalfantasyxiv.com/"> <IoHome />
+                    <span>FFXIV Main page</span>
                     </Button>
                     </li>
                     <li>
-                    <Button color="primary">
+                    <Button href="https://www.xivmodarchive.com/"> <BiArchive />
                     FFXIV ModArachive
                     </Button>
                     </li>
                     <li>
-                    <Button color="danger">
+                    <Button href="https://www.nexusmods.com/finalfantasy14"> <VscFileSubmodule />
                     Nexus mods
                     </Button>
                     </li>
-                
             </ul>
         </div>
     )
