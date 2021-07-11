00ee0622e67c006ef093e789281e569c847e22fa
Nate Phelps <bulletmagnet.123@gmail.com>
2021-07-10 21:43:22 -0700

    Got the game config in its own js file

--- src/App.js
+++ src/App.js
@@ -8,44 +8,15 @@ import { Home } from './Home'
 import { About } from './About'
 import { Contact } from './Contact'
 import { NoMatch } from './NoMatch'
-import { render } from '@testing-library/react';
 import { Layout } from './components/Layout'
 import { NavigationBar } from './components/NavBar'
 import { Jumbotron } from './components/Jumbotron'
 import SideNav from './components/SideNav';
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
-import { Scene0 } from './Scene0'
-
-let config = {
-  width: 1920,
-  height: 1080,
-  backgroundColor: '#FFFFF',
-  autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-  type: Phaser.AUTO,
-  scene: [Scene0],
-  parent: 'index',
-  physics: {
-      default: `arcade`,
-      arcade: {
-        gravity: { y: 200 },
-        debug: true
-      }
-  },
-  platforms: true,
-  groundPlat: true,
-  player: true,
-  cursors: true
-}
-let player;
-let platforms;
-let groundPlat;
-let game = new Phaser.Game(config);
-let cursors;
-const speed = 200;
-const speedDiag = speed * (1/1.44);
-
+import { gameconfig } from './GameConfig'
 
 class App extends Component {
+  
   render(){
     return (
       <React.Fragment>
@@ -63,11 +34,11 @@ class App extends Component {
                   <Route component={NoMatch} />
                 </Switch>
               </CSSTransition>
+              <IonPhaser game={gameconfig} />
             </TransitionGroup>
           </Router>
         </Layout>
       </React.Fragment>
-      
     )
   }
 }
