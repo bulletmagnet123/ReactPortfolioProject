99700495ba108174ffe6d7ee75c7693d56909aff
Bulletmagnet <bulletmagnet.123@gmail.com>
2023-06-22 16:04:21 -0700

    FINALLY after rolling back changes its all functional again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/HomeComponent'
 import About from './components/About'
 import Contact from './components/Contact'
